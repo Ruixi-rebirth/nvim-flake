8707c0d8734927a0581c0d597b855bb55fd93ba4
Ruixi-rebirth <ruixirebirth@gmail.com>
2024-04-24 22:43:39 +0800

    u

--- config/base/extraConfigLua.nix
+++ config/base/extraConfigLua.nix
@@ -6,10 +6,21 @@
       -- Allow moving between lines using arrow keys and h/l when at the start/end of a line
       vim.cmd("set whichwrap+=<,>,[,],h,l")
 
-      local local_nvimrc = vim.fn.getcwd()..'/.nvim.lua'
-      if vim.loop.fs_stat(local_nvimrc) then
-        vim.cmd('source '..local_nvimrc)
+      local util = require 'lspconfig.util'
+      local function load_project_nvim_config()
+          local root_dir = function(fname)
+              return util.find_git_ancestor(fname)
+          end
+          local current_file = vim.fn.expand('%:p')
+          local project_root = root_dir(current_file)
+          if project_root then
+              local nvim_config_path = project_root .. '/.nvim.lua'
+              if vim.loop.fs_stat(nvim_config_path) then
+                  vim.cmd('source ' .. nvim_config_path)
+              end
+          end
       end
+      load_project_nvim_config()
     '';
   };
 }
