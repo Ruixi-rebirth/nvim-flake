9136fb91a3f844cd8adedbeb154d0784414c4224
Ruixi-rebirth <ruixirebirth@gmail.com>
2024-04-27 15:19:48 +0800

    Initial Commit

---
+++ config/lazy_plugins/nvim-cmp.nix
@@ -0,0 +1,137 @@
+{ pkgs, ... }:
+{
+  pkg = pkgs.vimPlugins.nvim-cmp;
+  lazy = false;
+  event = "InsertEnter";
+  dependencies = with pkgs.vimPlugins; [
+    friendly-snippets
+    cmp-nvim-lsp
+    cmp-buffer
+    luasnip
+    cmp_luasnip
+    cmp-nvim-lua
+    cmp-path
+    cmp-cmdline
+  ];
+  config = ''
+    function()
+       local cmp_status_ok, cmp = pcall(require, "cmp")
+       if not cmp_status_ok then
+          return
+       end
+       local snip_status_ok, luasnip = pcall(require, "luasnip")
+       if not snip_status_ok then
+          return
+       end
+
+       require("luasnip/loaders/from_vscode").lazy_load()
+
+       local kind_icons = {
+          Text = "󰊄",
+          Method = "",
+          Function = "󰡱",
+          Constructor = "",
+          Field = "",
+          Variable = "󱀍",
+          Class = "",
+          Interface = "",
+          Module = "󰕳",
+          Property = "",
+          Unit = "",
+          Value = "",
+          Enum = "",
+          Keyword = "",
+          Snippet = "",
+          Color = "",
+          File = "",
+          Reference = "",
+          Folder = "",
+          EnumMember = "",
+          Constant = "",
+          Struct = "",
+          Event = "",
+          Operator = "",
+          TypeParameter = "",
+       }
+       -- find more here: https://www.nerdfonts.com/cheat-sheet
+
+       cmp.setup({
+          snippet = {
+             expand = function(args)
+             luasnip.lsp_expand(args.body) -- For `luasnip` users.
+             end,
+          },
+          mapping = cmp.mapping.preset.insert({
+             ["<C-u>"] = cmp.mapping.scroll_docs(-4), -- Up
+             ["<C-d>"] = cmp.mapping.scroll_docs(4), -- Down
+             ["<C-Space>"] = cmp.mapping.complete(),
+             ["<CR>"] = cmp.mapping.confirm({
+                select = true,
+             }),
+             ["<Tab>"] = cmp.mapping(function(fallback)
+             if cmp.visible() then
+                cmp.select_next_item()
+             elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+             else
+                fallback()
+             end
+             end, { "i", "s" }),
+             ["<S-Tab>"] = cmp.mapping(function(fallback)
+             if cmp.visible() then
+                cmp.select_prev_item()
+             elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+             else
+                fallback()
+             end
+             end, { "i", "s" }),
+          }),
+          formatting = {
+             fields = { "kind", "abbr", "menu" },
+             format = function(entry, vim_item)
+             -- Kind icons
+             vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+             -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+             vim_item.menu = ({
+                path = "[Path]",
+                nvim_lua = "[NVIM_LUA]",
+                nvim_lsp = "[LSP]",
+                luasnip = "[Snippet]",
+                buffer = "[Buffer]",
+             })[entry.source.name]
+             return vim_item
+             end,
+          },
+          sources = {
+             { name = "path" },
+             { name = "nvim_lua" },
+             { name = "nvim_lsp" },
+             { name = "luasnip" },
+             { name = "buffer" },
+          },
+          confirm_opts = {
+             behavior = cmp.ConfirmBehavior.Replace,
+             select = false,
+          },
+          window = {
+             completion = cmp.config.window.bordered(),
+             documentation = cmp.config.window.bordered(),
+          },
+          experimental = {
+             ghost_text = false,
+             native_menu = false,
+          },
+       })
+
+       cmp.setup.cmdline(":", {
+          mapping = cmp.mapping.preset.cmdline(),
+          sources = cmp.config.sources({
+             { name = "path" },
+          }, {
+          { name = "cmdline" },
+       }),
+    })
+    end
+  '';
+}
