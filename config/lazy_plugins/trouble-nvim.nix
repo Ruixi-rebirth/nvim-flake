39eb90241cff120ac8c9d9c3e8ce949fc998f2a3
Ruixi-rebirth <ruixirebirth@gmail.com>
2024-07-26 16:22:35 +0800

    update nvim-trouble keybinds

--- config/lazy_plugins/trouble-nvim.nix
+++ config/lazy_plugins/trouble-nvim.nix
@@ -4,6 +4,11 @@
   lazy = true;
   dependencies = with pkgs.vimPlugins; [ nvim-web-devicons ];
   keys = helpers.mkRaw ''{
-    { "tr", "<cmd>TroubleToggle<cr>>", desc = "toggle trouble" }
+    { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
+    { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)" },
+    { "<leader>cs", "<cmd>Trouble symbols toggle focus=false<cr>", desc = "Symbols (Trouble)" },
+    { "<leader>cl", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", desc = "LSP Definitions / references / ... (Trouble)" },
+    { "<leader>xL", "<cmd>Trouble loclist toggle<cr>", desc = "Location List (Trouble)" },
+    { "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List (Trouble)" }
   }'';
 }
