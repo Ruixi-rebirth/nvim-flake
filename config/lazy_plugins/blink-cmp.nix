cd063d066d85d13a5fb5daefa573fe7e2ee0e84b
Ruixi-rebirth <ruixirebirth@gmail.com>
2024-12-28 00:32:48 +0800

    init

---
+++ config/lazy_plugins/blink-cmp.nix
@@ -0,0 +1,419 @@
+{
+  pkgs,
+  inputs,
+  helpers,
+  ...
+}:
+let
+  minuet-ai = pkgs.vimUtils.buildVimPlugin {
+    name = "minuet-ai";
+    src = inputs.minuet-ai-nvim;
+    doCheck = false;
+  };
+in
+{
+  pkg = inputs.blink-cmp.packages.${pkgs.system}.default;
+  lazy = true;
+  event = "InsertEnter";
+  dependencies =
+    with pkgs.vimPlugins;
+    [
+      friendly-snippets
+      luasnip
+      {
+        pkg = pkgs.vimPlugins.blink-compat;
+        lazy = true;
+        opts = { };
+      }
+      {
+        pkg = minuet-ai;
+        dependencies = with pkgs.vimPlugins; [ plenary-nvim ];
+        config = ''
+          function()
+            -- you can use deepseek with both openai_fim_compatible or openai_compatible provider
+            require("minuet").setup({
+              provider = "openai_fim_compatible",
+              provider_options = {
+                openai_fim_compatible = {
+                  api_key = 'OPENAI_API_KEY',
+                  name = "deepseek",
+                  optional = {
+                    max_tokens = 4096,
+                    top_p = 0.9,
+                  },
+                },
+                openai_compatible = {
+                  api_key = 'OPENAI_API_KEY',
+                  end_point = "https://api.deepseek.com/v1/chat/completions",
+                  name = "deepseek",
+                  optional = {
+                    max_tokens = 4096,
+                    top_p = 0.9,
+                  },
+                },
+              },
+            })
+          end
+        '';
+      }
+    ]
+    ++ [
+      # nvim-cmp source
+      blink-compat
+      {
+        pkg = pkgs.vimPlugins.copilot-cmp;
+        dependencies = with pkgs.vimPlugins; [
+          {
+            pkg = pkgs.vimPlugins.copilot-lua;
+            cmd = "Copilot";
+            event = "InsertEnter";
+            config = ''
+              function()
+                require("copilot").setup({
+                  suggestion = { enabled = false },
+                  panel = { enabled = false },
+                  copilot_node_command = "${pkgs.nodejs-18_x}/bin/node",
+                  filetypes = { ["*"] = true, },
+                })
+              end
+            '';
+          }
+        ];
+        config = ''
+          function()
+            require("copilot_cmp").setup({
+              event = { "InsertEnter", "LspAttach" },
+              fix_pairs = true,
+            })
+          end
+        '';
+      }
+      {
+        pkg = pkgs.vimPlugins.cmp-spell;
+        config = ''
+          function()
+            vim.cmd('highlight clear SpellBad')
+            vim.cmd('highlight clear SpellCap')
+            vim.cmd('highlight clear SpellLocal')
+            vim.cmd('highlight clear SpellRare')
+          end
+        '';
+      }
+      cmp-rg
+      cmp-calc
+    ];
+  config = ''
+    function()
+      require("blink.cmp").setup({
+        completion = {
+          accept = {
+            auto_brackets = {
+              enabled = true,
+            },
+          },
+          list = {
+            selection = { preselect = false, auto_insert = true },
+            cycle = {
+              -- When `true`, calling `select_next` at the *bottom* of the completion list
+              -- will select the *first* completion item.
+              from_bottom = true,
+              -- When `true`, calling `select_prev` at the *top* of the completion list
+              -- will select the *last* completion item.
+              from_top = true,
+            },
+          },
+          menu = {
+            enabled = true,
+            border = "rounded",
+            winblend = 0,
+            auto_show = true,
+            winhighlight = "Normal:_BlinkCmpMenu,FloatBorder:_BlinkCmpMenuBorder,CursorLine:_BlinkCmpMenuSelection,Search:None",
+            draw = {
+              align_to = 'label',
+              gap = 1,
+              padding = 0,
+              treesitter = { "lsp" },
+              columns = {
+                { "kind_icon" },
+                { "label", "label_description", gap = 1 },
+                { "source_name" },
+              },
+              components = {
+                kind_icon = {
+                  ellipsis = false,
+                  text = function(ctx)
+                    return ctx.kind_icon .. ctx.icon_gap
+                  end,
+                  highlight = function(ctx)
+                    return require("blink.cmp.completion.windows.render.tailwind").get_hl(ctx) or "BlinkCmpKind" .. ctx.kind
+                  end,
+                },
+                kind = {
+                  ellipsis = false,
+                  width = { fill = true },
+                  text = function(ctx)
+                    return ctx.kind
+                  end,
+                  highlight = function(ctx)
+                    return require("blink.cmp.completion.windows.render.tailwind").get_hl(ctx) or "BlinkCmpKind" .. ctx.kind
+                  end,
+                },
+                label = {
+                  width = { fill = true, max = 60 },
+                  text = function(ctx)
+                    return ctx.label .. ctx.label_detail
+                  end,
+                  highlight = function(ctx)
+                    -- label and label details
+                    local highlights = {
+                      { 0, #ctx.label, group = ctx.deprecated and "BlinkCmpLabelDeprecated" or "BlinkCmpLabel" },
+                    }
+                    if ctx.label_detail then
+                      table.insert(highlights, { #ctx.label, #ctx.label + #ctx.label_detail, group = "BlinkCmpLabelDetail" })
+                    end
+
+                    -- characters matched on the label by the fuzzy matcher
+                    for _, idx in ipairs(ctx.label_matched_indices) do
+                      table.insert(highlights, { idx, idx + 1, group = "BlinkCmpLabelMatch" })
+                    end
+
+                    return highlights
+                  end,
+                },
+                label_description = {
+                  width = { max = 30 },
+                  text = function(ctx)
+                    return ctx.label_description
+                  end,
+                  highlight = "BlinkCmpLabelDescription",
+                },
+                source_name = {
+                  width = { fill = true, max = 30 },
+                  text = function(ctx)
+                    return ctx.source_name
+                  end,
+                  highlight = "BlinkCmpSource",
+                },
+              },
+            },
+          },
+          keyword = {
+            range = full,
+          },
+          trigger = {
+            prefetch_on_insert = false,
+            show_on_keyword = true,
+            show_on_trigger_character = true,
+            show_on_insert_on_trigger_character = true,
+            show_on_accept_on_trigger_character = true,
+          },
+          documentation = {
+            auto_show = true,
+            auto_show_delay_ms = 100,
+            update_delay_ms = 50,
+            treesitter_highlighting = true,
+            window = {
+              border = "rounded",
+              winblend = 0,
+              winhighlight = "Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,EndOfBuffer:BlinkCmpDoc",
+            },
+          },
+          ghost_text = {
+            enabled = false,
+          },
+        },
+        signature = {
+          enabled = true,
+          window = {
+            border = "double",
+            winblend = 0,
+            winhighlight = "Normal:BlinkCmpSignatureHelp,FloatBorder:BlinkCmpSignatureHelpBorder",
+            treesitter_highlighting = true,
+            show_documentation = true,
+          },
+          trigger = {
+            blocked_trigger_characters = {},
+            blocked_retrigger_characters = {},
+            -- When true, will show the signature help window when the cursor comes after a trigger character when entering insert mode
+            show_on_insert_on_trigger_character = true,
+          },
+        },
+        keymap = {
+          preset = none,
+          ["<cr>"] = { "accept", "fallback" },
+          ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+          ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+          ["<c-u>"] = { "scroll_documentation_up", "fallback" },
+          ["<c-d>"] = { "scroll_documentation_down", "fallback" },
+          ["<c-.>"] = {
+            function(cmp)
+              if cmp.is_visible() then
+                cmp.cancel()
+              else
+                cmp.show()
+              end
+            end,
+          },
+        },
+        snippets = {
+          preset = 'default', -- | default | luasnip |
+        },
+        sources = {
+          default = { "lsp", "path", "snippets", "cmdline", "buffer", "copilot", "spell", "calc", "rg", "dadbod", "minuet" },
+          providers = {
+            lsp = { score_offset = 5, },
+            snippets = { score_offset = 4, },
+            minuet = {
+              name = 'minuet',
+              module = 'minuet.blink',
+              async = true,
+              score_offset = 101, -- Gives minuet higher priority among suggestions
+              transform_items = function(_, items)
+                local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+                local kind_idx = #CompletionItemKind + 1
+                CompletionItemKind[kind_idx] = "Minuet"
+                for _, item in ipairs(items) do
+                  item.kind = kind_idx
+                  item.source_name = "Minuet"
+                end
+                return items
+              end,
+            },
+            cmdline = {
+              enabled = function()
+                return vim.fn.mode() == "c"
+              end,
+              name = "cmdline",
+              module = "blink.cmp.sources.cmdline",
+              score_offset = 5,
+              transform_items = function(_, items)
+                local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+                local kind_idx = #CompletionItemKind + 1
+                CompletionItemKind[kind_idx] = "Cmdline"
+                for _, item in ipairs(items) do
+                  item.kind = kind_idx
+                  item.source_name = "Cmdline"
+                end
+                return items
+              end,
+            },
+            copilot = {
+              enabled = true,
+              name = "copilot", -- same as source_name in nvim-cmp
+              module = "blink.compat.source",
+              score_offset = 100,
+              async = true,
+              transform_items = function(_, items)
+                local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+                local kind_idx = #CompletionItemKind + 1
+                CompletionItemKind[kind_idx] = "Copilot"
+                for _, item in ipairs(items) do
+                  item.kind = kind_idx
+                  item.source_name = "Copilot"
+                end
+                return items
+              end,
+            },
+            spell = {
+              enabled = true,
+              name = "spell", -- same as source_name in nvim-cmp
+              module = "blink.compat.source",
+              async = true,
+              score_offset = -5,
+              transform_items = function(_, items)
+                local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+                local kind_idx = #CompletionItemKind + 1
+                CompletionItemKind[kind_idx] = "Spell"
+                for _, item in ipairs(items) do
+                  item.kind = kind_idx
+                  item.source_name = "Spell"
+                end
+                return items
+              end,
+            },
+            calc = {
+              enabled = true,
+              name = "calc", -- same as source_name in nvim-cmp
+              module = "blink.compat.source",
+              async = true,
+              score_offset = -5,
+              transform_items = function(_, items)
+                local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+                local kind_idx = #CompletionItemKind + 1
+                CompletionItemKind[kind_idx] = "Calc"
+                for _, item in ipairs(items) do
+                  item.kind = kind_idx
+                  item.source_name = "Calc"
+                end
+                return items
+              end,
+            },
+            rg = {
+              enabled = true,
+              name = "rg", -- same as source_name in nvim-cmp
+              module = "blink.compat.source",
+              async = true,
+              score_offset = -5,
+              transform_items = function(_, items)
+                local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+                local kind_idx = #CompletionItemKind + 1
+                CompletionItemKind[kind_idx] = "Rg"
+                for _, item in ipairs(items) do
+                  item.kind = kind_idx
+                  item.source_name = "Rg"
+                end
+                return items
+              end,
+            },
+            dadbod = {
+              name = "Dadbod",
+              module = "vim_dadbod_completion.blink",
+              score_offset = -3,
+            },
+          },
+        },
+        appearance = {
+          nerd_font_variant = "normal",
+          kind_icons = {
+            Text = "󰊄",
+            Method = "",
+            Function = "󰡱",
+            Constructor = "",
+            Field = "",
+            Variable = "󱀍",
+            Class = "",
+            Interface = "",
+            Module = "󰕳",
+            Property = "",
+            Unit = "",
+            Value = "",
+            Enum = "",
+            Keyword = "",
+            Snippet = "",
+            Color = "",
+            File = "",
+            Reference = "",
+            Folder = "",
+            EnumMember = "",
+            Constant = "",
+            Struct = "",
+            Event = "",
+            Operator = "",
+            TypeParameter = "",
+            Cmdline = "",
+            Copilot = "",
+            Spell = "󰓆",
+            Calc = "",
+            Rg = "",
+            Minuet = "󱗻",
+          },
+        },
+        fuzzy = {
+          prebuilt_binaries = {
+            download = false,
+          },
+        }
+      })
+    end
+  '';
+}
---
+++ config/lazy_plugins/nvim-lspconfig.nix
@@ -0,0 +1,242 @@
+{ pkgs, inputs, ... }:
+{
+  pkg = pkgs.vimPlugins.nvim-lspconfig;
+  dependencies = with pkgs.vimPlugins; [
+    # nvim-cmp
+    inputs.blink-cmp.packages.${pkgs.system}.default
+  ];
+  config = ''
+    function()
+      -- Add additional capabilities supported by blink-cmp
+      local cmp_capabilities = require('blink.cmp').get_lsp_capabilities()
+      -- Add additional capabilities supported by nvim-cmp
+      -- local cmp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+      -- nvim hasn't added foldingRange to default capabilities, users must add it manually
+      local lsp_capabilities = vim.lsp.protocol.make_client_capabilities()
+      local capabilities = vim.tbl_deep_extend("force", lsp_capabilities, cmp_capabilities)
+      capabilities.textDocument.foldingRange = {
+        dynamicRegistration = false,
+        lineFoldingOnly = true,
+      }
+
+      --Change diagnostic symbols in the sign column (gutter)
+      local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+      for type, icon in pairs(signs) do
+        local hl = "DiagnosticSign" .. type
+        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+      end
+      vim.diagnostic.config({
+        virtual_text = false,
+        signs = true,
+        underline = true,
+        update_in_insert = true,
+        severity_sort = false,
+      })
+
+      local on_attach_common = function(client, bufnr)
+        vim.api.nvim_create_autocmd("CursorHold", {
+          buffer = bufnr,
+          callback = function()
+            local opts = {
+              focusable = false,
+              close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+              border = "rounded",
+              source = "always",
+              prefix = " ",
+              scope = "line",
+            }
+            vim.diagnostic.show()
+            vim.diagnostic.open_float(nil, opts)
+          end,
+        })
+
+        -- keymap
+        local opts = { noremap = true, silent = true, buffer = bufnr }
+
+        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, vim.tbl_extend("force", { desc = "Go to definition" }, opts or {}))
+        vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, vim.tbl_extend("force", { desc = "Go to declaration" }, opts or {}))
+        vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, vim.tbl_extend("force", { desc = "Go to implementation" }, opts or {}))
+        vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, vim.tbl_extend("force", { desc = "Find references" }, opts or {}))
+        vim.keymap.set("n", "<leader>D", function() vim.lsp.buf.type_definition() end, vim.tbl_extend("force", { desc = "Go to type definition" }, opts or {}))
+        vim.keymap.set("n", "K", function() return vim.lsp.buf.hover() end, vim.tbl_extend("force", { desc = "Hover documentation" }, opts or {}))
+        vim.keymap.set("n", "<leader>s", function() vim.lsp.buf.signature_help() end, vim.tbl_extend("force", { desc = "Show signature help" }, opts or {}))
+        vim.keymap.set("n", "<leader>wa", function() vim.lsp.buf.add_workspace_folder() end, vim.tbl_extend("force", { desc = "Add workspace folder" }, opts or {}))
+        vim.keymap.set("n", "<leader>wr", function() vim.lsp.buf.remove_workspace_folder() end, vim.tbl_extend("force", { desc = "Remove workspace folder" }, opts or {}))
+        vim.keymap.set("n", "<leader>wl", function()
+          print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, vim.tbl_extend("force", { desc = "List workspace folders" }, opts or {}))
+        vim.keymap.set("n", "<leader>n", function() vim.lsp.buf.rename() end, vim.tbl_extend("force", { desc = "Rename symbol" }, opts or {}))
+        vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, vim.tbl_extend("force", { desc = "Code actions" }, opts or {}))
+        vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format() end, vim.tbl_extend("force", { desc = "Format code" }, opts or {}))
+        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, vim.tbl_extend("force", { desc = "Go to previous diagnostic" }, opts or {}))
+        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, vim.tbl_extend("force", { desc = "Go to next diagnostic" }, opts or {}))
+        vim.keymap.set("n", "<leader>L", function()
+          print(vim.inspect(vim.lsp.get_clients({ bufnr = bufnr })))
+        end, vim.tbl_extend("force", { desc = "List LSP clients" }, opts or {}))
+
+        -- vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]] -- use none-ls
+      end
+
+      local nvim_lsp = require("lspconfig")
+      ---------------------
+      -- setup languages --
+      ---------------------
+      -- nix
+      nvim_lsp.nixd.setup({
+        cmd = { "${pkgs.nixd}/bin/nixd" },
+        on_attach = on_attach_common(),
+        capabilities = capabilities,
+      })
+      -- golang
+      nvim_lsp["gopls"].setup({
+        cmd = { "${pkgs.gopls}/bin/gopls" },
+        on_attach = on_attach_common(),
+        capabilities = capabilities,
+        settings = {
+          gopls = {
+            experimentalPostfixCompletions = true,
+            analyses = {
+              unusedparams = true,
+              shadow = true,
+            },
+            staticcheck = true,
+            gofumpt = true,
+            hints = {
+              assignVariableTypes = true,
+              compositeLiteralFields = true,
+              compositeLiteralTypes = true,
+              constantValues = true,
+              functionTypeParameters = true,
+              parameterNames = true,
+              rangeVariableTypes = true,
+            },
+          },
+        },
+        init_options = {
+          usePlaceholders = true,
+        },
+      })
+      --python
+      nvim_lsp.pyright.setup({
+        cmd = { "${pkgs.pyright}/bin/pyright-langserver", "--stdio" },
+        on_attach = on_attach_common(),
+        capabilities = capabilities,
+        settings = {
+          python = {
+            analysis = {
+              autoSearchPaths = true,
+              diagnosticMode = "workspace",
+              useLibraryCodeForTypes = true,
+              typeCheckingMode = "off",
+            },
+          },
+        },
+      })
+
+      --lua
+      nvim_lsp.lua_ls.setup({
+        cmd = { "${pkgs.lua-language-server}/bin/lua-language-server" },
+        on_attach = on_attach_common(),
+        capabilities = capabilities,
+        settings = {
+          Lua = {
+            runtime = {
+              -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+              version = "LuaJIT",
+            },
+            diagnostics = {
+              -- Get the language server to recognize the `vim` global
+              globals = { "vim" },
+            },
+            workspace = {
+              -- Make the server aware of Neovim runtime files
+              library = vim.api.nvim_get_runtime_file("", true),
+              checkThirdParty = false,
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+              enable = false,
+            },
+          },
+        },
+      })
+
+      nvim_lsp.rust_analyzer.setup({
+        cmd = { "${pkgs.rust-analyzer}/bin/rust-analyzer" },
+        on_attach = function(client, bufnr)
+          on_attach_common(client, bufnr)
+          vim.cmd([[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]])
+        end,
+        capabilities = capabilities,
+      })
+      nvim_lsp.html.setup({
+        cmd = { "${pkgs.vscode-langservers-extracted}/bin/vscode-html-language-server", "--stdio" },
+        on_attach = on_attach_common(),
+        capabilities = capabilities,
+      })
+
+      nvim_lsp.cssls.setup({
+        cmd = { "${pkgs.vscode-langservers-extracted}/bin/vscode-css-language-server", "--stdio" },
+        on_attach = on_attach_common(),
+        capabilities = capabilities,
+      })
+
+      nvim_lsp.ts_ls.setup({
+        cmd = { "${pkgs.typescript-language-server}/bin/typescript-language-server", "--stdio" },
+        on_attach = on_attach_common(),
+        capabilities = capabilities,
+      })
+
+      nvim_lsp.volar.setup({
+        cmd = { "${pkgs.vue-language-server}/bin/vue-language-server", "--stdio" },
+        on_attach = on_attach_common(),
+        capabilities = capabilities,
+      })
+
+      nvim_lsp.bashls.setup({
+        cmd = { "${pkgs.bash-language-server}/bin/bash-language-server", "start" },
+        on_attach = on_attach_common(),
+        capabilities = capabilities,
+      })
+
+      nvim_lsp.hls.setup({
+        cmd = { "${pkgs.haskell-language-server}/bin/haskell-language-server-wrapper", "--lsp" },
+        on_attach = function(client, bufnr)
+          on_attach_common(client, bufnr)
+          vim.cmd([[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]])
+        end,
+        capabilities = capabilities,
+      })
+
+      -- show diagnostics when InsertLeave
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "go", "rust", "nix", "haskell" },
+        callback = function(args)
+          vim.api.nvim_create_autocmd("DiagnosticChanged", {
+            buffer = args.buf,
+            callback = function()
+              vim.diagnostic.hide()
+            end,
+          })
+          vim.api.nvim_create_autocmd({ "InsertLeave", "BufWritePost" }, {
+            buffer = args.buf,
+            callback = function()
+              vim.diagnostic.show()
+            end,
+          })
+        end,
+      })
+
+      _G.toggle_inlay_hints = function()
+        vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+      end
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "rust", "go", "nix" },
+        callback = function()
+          vim.api.nvim_buf_create_user_command(0, "InlayHintsToggle", _G.toggle_inlay_hints, {})
+        end,
+      })
+    end
+  '';
+}
