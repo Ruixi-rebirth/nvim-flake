8c7a3da9ec906d4dd15d30a3638a626af42b2848
Ruixi-rebirth <ruixirebirth@gmail.com>
2025-03-02 20:26:16 +0800

    init

---
+++ config/lazy_plugins/nvim-ts-context-commentstring.nix
@@ -0,0 +1,18 @@
+{ pkgs, helpers, ... }:
+{
+  pkg = pkgs.vimPlugins.nvim-ts-context-commentstring;
+  lazy = true;
+  event = "BufRead";
+  init = ''
+    function()
+      vim.g.skip_ts_context_commentstring_module = true
+    end
+  '';
+  config = ''
+    function()
+      require("ts_context_commentstring").setup({
+        enable_autocmd = false,
+      })
+    end
+  '';
+}
