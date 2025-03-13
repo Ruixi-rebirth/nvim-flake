d7c52855903f0e858d10e664c26b94a41947bcea
Ruixi-rebirth <ruixirebirth@gmail.com>
2025-03-13 20:59:41 +0800

    u

--- config/lazy_plugins/img-clip-nvim.nix
+++ config/lazy_plugins/img-clip-nvim.nix
@@ -10,8 +10,7 @@
       drag_and_drop = {
         insert_mode = true;
       };
-      # required for Windows users
-      use_absolute_path = true;
+      use_absolute_path = false;
     };
   };
   opts = { };
